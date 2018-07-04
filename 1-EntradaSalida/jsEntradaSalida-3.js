47404f3b2e299b136baa9529416e66e2e97a53c5
Lucasvaldiviezo <40808728+Lucasvaldiviezo@users.noreply.github.com>
2018-07-03 21:57:02 -0300

    Ejercicio 3
    Ejercicio 3 ya completo.

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,8 +2,11 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombre;
-	
+	var nombre = document.getElementById('elNombre').value;
+
+	alert(nombre);
+
+
 }
 
 
