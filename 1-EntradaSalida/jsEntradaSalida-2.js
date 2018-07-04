eb4a3cea47daa3c6b90bdf6ac3c7b16db36f50a2
Lucasvaldiviezo <40808728+Lucasvaldiviezo@users.noreply.github.com>
2018-07-03 21:50:38 -0300

    Primer Guardado
    Son los ejercicios del 1 al 4

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,21 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//var person = prompt("Please enter your name", "Harry Potter");
+//comentario de una linea
+/*
+Esto esto un comentario en bloque, sirve para tomar apuntes
+
+
+*/
+
+var nombre;
+//alert("nombre");
+//nombre="Su nombre";
+
+nombre=prompt("ingrese nombre","Natalia Natalia");
+
+alert("Su nombre es:"+nombre);
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,6 +2,7 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	var nombre;
 	
 }
 
