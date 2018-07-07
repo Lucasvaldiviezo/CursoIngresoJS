1f9f065e35c459d2e7b69825d80c964ed5d9aacf
Lucasvaldiviezo <40808728+Lucasvaldiviezo@users.noreply.github.com>
2018-07-06 21:56:45 -0300

    Ejercicios del 1 al 3 del Recuperatorio
    Ejercicios del Recuperatorio del 1 al 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuento;
+	var preciocondescuento;
+	var iva;
+	var precioconiva;
+
+	precio = prompt("Ingrese precio");
+	
+	precio = parseInt(precio);
+
+	porcentaje=prompt("Ingrese porcentaje");
+
+	porcentaje=parseInt(porcentaje);
+
+	descuento=precio * porcentaje/100;
+	
+	preciocondescuento= precio - descuento;
+	
+	iva= preciocondescuento * 21/100;
+	
+	precioconiva = preciocondescuento + iva;
+
+	alert("Su descuento es de: " + descuento + " y su precio con descuento es: " + preciocondescuento + " Su iva es de: " + iva);
+
+	document.getElementById('elPrecioFinal').value=precioconiva;
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var area;
+	var perimetro;
+
+	base = prompt("Ingrese la Base");
+	base = parseInt(base);
+
+	altura=prompt("Ingrese la Altura");
+	altura=parseInt(altura);
+
+	area = base * altura/2
+	perimetro= base * 3;
+
+	alert("El area del triangulo es: " + area + " y su perimetro: " + perimetro);
+
 
 }
