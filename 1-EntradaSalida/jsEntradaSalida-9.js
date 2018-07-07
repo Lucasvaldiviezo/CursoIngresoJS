b92cb2c0ba9a7f7c4d105b3ef10582382fc118e2
Lucasvaldiviezo <40808728+Lucasvaldiviezo@users.noreply.github.com>
2018-07-06 21:55:27 -0300

    Ejercicios del 5 al 10
    Ejercicios del 5 al 10 ya completos

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,24 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	
+	//(sueldo x 10) / 100 o sueldo x 0,10.
+	//sueldo + (sueldo x 10) / 100
+	//sueldo(1 + 0,10) es factor comun. --> sueldo x 1,10 para aumento.
+	// para un descuento de de 25%  ---> sueldo x 0,75.
+
+	var sueldo;
+	var aumento;
+	var resultado;
+
+	sueldo = document.getElementById('sueldo').value;
+	sueldo = parseInt(sueldo);
+
+	aumento = (sueldo * 10)/100;
+
+	resultado = sueldo + aumento;
+
+	document.getElementById('resultado').value = resultado;
+
+
+
 }
