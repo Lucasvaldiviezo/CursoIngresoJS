78317537c60d11f282c3965ceb2920c0e688131a
Lucasvaldiviezo <40808728+Lucasvaldiviezo@users.noreply.github.com>
2018-07-13 22:07:27 -0300

    Recuperatorio 4 Listo
    Ejercicio del Recuperatorio 4 Listo

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,43 @@
 function mostrar()
 {
+	var numero1;
+	var numero2;
+	var division;
+	var suma;
+	var respuesta;
 
+	numero1=prompt("ingrese numero 1");
+	numero2=prompt("ingrese numero 2");
+
+	
+	if(numero1==numero2)
+	{
+		respuesta=numero1 +  numero2;
+	}
+	else
+	{
+		if(numero1 > numero2)
+		{
+			
+			respuesta=numero1/numero2;
+		}
+		else
+		{
+			numero1=parseInt(numero1);
+			numero2=parseInt(numero2);
+
+			respuesta=numero1+numero2;
+
+			if(respuesta < 50)
+			{
+					respuesta="La suma es: " + respuesta + " y es menor a 50.";
+			}
+			
+
+		}
+
+	} 
+
+	alert(respuesta);
+	
 }
