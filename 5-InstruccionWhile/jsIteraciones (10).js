5bb1ab7887988874a3089774720c4b74847d5f07
Lucasvaldiviezo <40808728+Lucasvaldiviezo@users.noreply.github.com>
2018-07-27 19:47:40 -0300

    Ejercicio 10 de While Completo
    Lo hice!

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,16 +2,73 @@ function mostrar()
 {
 
 	var contador=0;
+	var numeroIngresado;
+	var totalpositivo = 0;
+	var totalnegativo = 0;
+	var contadorpositivos = 0;
+	var contadornegativos = 0;
+	var contadorceros = 0;
+	var numerospares = 0;
+	var resto;
+	var promediopositivos;
+	var promedionegativos;
+	var diferencia;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
 
-	/*while(respuesta!="no")
+	while(respuesta!="no")
 	{
-			
-	}*/
+		numeroIngresado= prompt("Ingrese numero");
+		numeroIngresado=parseInt(numeroIngresado);
 
-	document.write("hola");
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado= prompt("Ingrese numero");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
+		
+		resto = numeroIngresado % 2;
+
+		if(numeroIngresado < 0)
+		{
+			contadornegativos++;
+			totalnegativo = totalnegativo + (numeroIngresado);
+		}else if(numeroIngresado > 0)
+		{
+			contadorpositivos++;
+			totalpositivo=totalpositivo + numeroIngresado;
+		}else
+		{
+			contadorceros++;
+		}
+
+		
+		if(resto == 0)
+		{
+			numerospares++;	
+		}
+
+		respuesta=prompt("Ingrese no para salir");	
+	}
+
+	promediopositivos = totalpositivo / contadorpositivos;
+	promedionegativos = totalnegativo / contadornegativos;
+	diferencia = totalpositivo - totalnegativo;
+
+	document.write("La suma de los negativos es: " + totalnegativo + "<br>"); 
+	document.write("La suma de los positivos es: " + totalpositivo + "<br>"); 
+	document.write("La cantidad de numeros positivos es: " + contadorpositivos + "<br>"); 
+	document.write("La cantidad de numeros negativos es: " + contadornegativos + "<br>"); 
+	document.write("La cantidad de ceros es: " + contadorceros + "<br>"); 
+	document.write("La cantidad de numeros pares es: " + numerospares + "<br>"); 
+	document.write("El promedio  de los numeros positivos es: " + promediopositivos + "<br>"); 
+	document.write("El promedio  de los numeros negativos es: " + promedionegativos + "<br>"); 
+	document.write("La diferencia entre  positivos y negativos es: " + diferencia + "<br>");
+
+
+
+	//document.write("numeroIngresado");
 
 
 }//FIN DE LA FUNCIÓN
